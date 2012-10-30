5a0f3f2d8de9c0f648420fa4de17979897e09cf9
Jon Gold <hello@designedbygold.com>
2012-10-30 21:47:36 +0000

    Initial Commit

---
+++ Stopwatch/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Stopwatch
+#
+#  Created by Jon Gold on 30/10/2012.
+#  Copyright 2012 Jon Gold. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ Stopwatch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stopwatch
+//
+//  Created by Jon Gold on 30/10/2012.
+//  Copyright (c) 2012 Jon Gold. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
